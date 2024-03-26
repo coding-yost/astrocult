6e3baa42a7ca044d368e75840c612cb1f0778746
coding-yost <77023930+coding-yost@users.noreply.github.com>
2024-03-25 17:59:11 -0700

    Stuff

--- Astrocultivator_Web_App/astrocultivators/routes.py
+++ Astrocultivator_Web_App/astrocultivators/routes.py
@@ -1,7 +1,15 @@
 from flask import render_template, url_for, flash, redirect
+import pandas as pd
 from astrocultivators import app
+
+# df = pd.read_csv('csv/test_seconds.csv')
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
+
+@app.route("/csv")
+def csv():
+    df_html = df.to_html()
+    return render_template('csv.html', data=df_html)
